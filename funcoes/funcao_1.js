d88a315eb2b89a13e5173fa76ab9e31a53be7a62
Henrique <henrique.thsantos.ths@gmail.com>
2021-05-10 19:40:47 -0300

    Básico sobre Função #1

---
+++ funcoes/funcao_1.js
@@ -0,0 +1,26 @@
+let a = 4;
+console.log(a);
+
+//Function Declaration
+function bomDia() {
+    console.log("Bom dia!");
+}
+
+bomDia();
+
+//Function expression
+const boaTarde = function () {
+    console.log("Boa tarde!");
+};
+
+boaTarde();
+
+function somar(a, b) {
+    return a + b;
+}
+
+let resultado = somar(3, 4, 5, 7, 8);
+console.log(resultado);
+
+resultado = somar(3);
+console.log(resultado);
